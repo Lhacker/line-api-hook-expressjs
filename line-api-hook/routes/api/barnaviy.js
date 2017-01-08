bfc609fe9fe1b40424128f84a77320167d3c1cff
Lhacker <starclown1@gmail.com>
2017-01-09 01:54:58 +0900

    add new line webhook as barnaviy.js

---
+++ line-api-hook/routes/api/barnaviy.js
@@ -0,0 +1,57 @@
+(function(module) {
+
+  const WebhookRequest = require('../common/line/webhook-request.js');
+  const LineReply = require('../common/line/reply-api.js');
+  const CHANNEL_SECRET_KEY = process.env.BARNAVIY_LINE_CHANNEL_SECRET_KEY;
+  const CHANNEL_ACCESS_TOKEN = process.env.BARNAVIY_LINE_CHANNEL_ACCESS_TOKEN;
+
+  const routeUrl = function(router) {
+    router.get('/barnaviy', function(req, res, next) {
+      res.json({"code": 0, "message": "I'm alive now"});
+    });
+
+    router.post('/barnaviy', function(req, res, next) {
+      // As Webhook API spec, we have to return 200 OK response
+      res.sendStatus(200);
+
+      try {
+        if (!WebhookRequest.isValid(req, CHANNEL_SECRET_KEY)) {
+          console.log('Bad request :' +
+                      'Requested header : ' + JSON.stringify(req.headers) + "\n" +
+                      'Requested data : ' + JSON.stringify(req.body));
+          return next();
+        }
+
+        for (let i = 0, l = req.body.events.length; i < l; i++) {
+          let webhookRequest = new WebhookRequest(req.body.events[i]);
+
+          if (!webhookRequest.type.match(`${WebhookRequest.CONSTANT.EVENT_TYPE.MESSAGE}`)) {
+            continue;
+          }
+
+          // make messages
+          var messages = [
+            {
+              "type": "text",
+              "text": webhookRequest.message.text
+            }
+          ];
+
+          let lineReply = new LineReply(CHANNEL_ACCESS_TOKEN);
+          lineReply.sendReplyAsync(webhookRequest.replyToken, messages);
+        }
+
+      } catch(e) {
+        console.log('Internal Server Error :' +
+                    'Requested header : ' + JSON.stringify(req.headers) + "\n" +
+                    'Requested data : ' + JSON.stringify(req.body) + "\n" +
+                    e);
+      }
+    });
+
+    return router;
+  };
+
+  module.exports = routeUrl;
+
+})(module);
--- line-api-hook/routes/api/index.js
+++ line-api-hook/routes/api/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router = require('./echo')(router);
+router = require('./barnaviy')(router);
 
 module.exports = router;
