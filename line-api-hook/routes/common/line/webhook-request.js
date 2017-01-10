a8653dbcaf918ddf9df299611572986347cfc514
Lhacker <starclown1@gmail.com>
2017-01-10 22:58:51 +0900

    refactor and add getter/setter

--- line-api-hook/routes/common/line/webhook-request.js
+++ line-api-hook/routes/common/line/webhook-request.js
@@ -2,14 +2,6 @@
 
   const crypto = require("crypto");
 
-  const CONSTANT = {
-    EVENT_TYPE: {
-      MESSAGE: 'message',
-      FOLLOW: 'follow',
-      POSTBACK: 'postback'
-    }
-  };
-
   const webhookRequest = function(event) {
     this.replyToken = event.replyToken;
     this.timestamp = event.timestamp;
@@ -20,18 +12,78 @@
         this.message = event.message;
         break;
       case 'postback':
-        this.postbackData = event.postback.data;
+        this.postback = event.postback;
         break;
+      case 'beacon':
+        this.beacon = beacon;
       default:
         break;
     }
   };
 
-  webhookRequest.CONSTANT = CONSTANT;
-
   webhookRequest.isValid = function(req, channelSecretKey) {
-    return req.headers['x-line-signature'] ==
+    return req.headers['x-line-signature'] ===
       crypto.createHmac('sha256', channelSecretKey).update(new Buffer(JSON.stringify(req.body), 'utf8')).digest('base64');
+  };
+
+  webhookRequest.prototype.fromUser = function() { return this.source.type === 'user'; };
+
+  webhookRequest.prototype.fromGroupChat = function() { return this.source.type === 'group'; };
+
+  webhookRequest.prototype.fromRoomChat = function() { return this.source.type === 'room'; };
+
+  webhookRequest.prototype.isMessageType = function() { return this.type === 'message'; };
+
+  webhookRequest.prototype.isFollowMessage = function() { return this.type === 'follow'; };
+
+  webhookRequest.prototype.isUnfollowMessage = function() { return this.type === 'unfollow'; };
+
+  webhookRequest.prototype.isJoinMessage = function() { return this.type === 'join'; };
+
+  webhookRequest.prototype.isLeaveMessage = function() { return this.type === 'leave'; };
+
+  webhookRequest.prototype.isPostBackedMessage = function() { return this.type === 'postback'; };
+
+  webhookRequest.prototype.isTextMessage = function() { return this.isMessageType() && this.message.type === 'text'; };
+
+  webhookRequest.prototype.isImageMessage = function() { return this.isMessageType() && this.message.type === 'image'; };
+
+  webhookRequest.prototype.isVideoMessage = function() { return this.isMessageType() && this.message.type === 'video'; };
+
+  webhookRequest.prototype.isStickerMessage = function() { return this.isMessageType() && this.message.type === 'sticker'; };
+
+  webhookRequest.prototype.isLocationMessage = function() { return this.isMessageType() && this.message.type === 'location'; };
+
+  webhookRequest.prototype.getReplyToken = function() { return this.replyToken; }
+
+  webhookRequest.prototype.getSourceUserId = function() {
+    if (!this.fromUser()) { return ''; }
+    return this.source.userId;
+  }
+
+  webhookRequest.prototype.getSourceGroupId = function() {
+    if (!this.fromGroupChat()) { return ''; }
+    return this.source.groupId;
+  }
+
+  webhookRequest.prototype.getSourceRoomId = function() {
+    if (!this.fromRoomChat()) { return ''; }
+    return this.source.roomId;
+  }
+
+  webhookRequest.prototype.getTextMessage = function() {
+    if (!this.isTextMessage()) { return ''; }
+    return this.message.text;
+  }
+
+  webhookRequest.prototype.getLocationMessage = function() {
+    if (!this.isLocationMessage()) { return {}; }
+    return this.message.location;
+  }
+
+  webhookRequest.prototype.getPostBackedData = function() {
+    if (!this.isPostBackedMessage()) { return ''; }
+    return this.postback.data;
   }
 
   module.exports = webhookRequest;
